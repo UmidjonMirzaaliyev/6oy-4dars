58bdca522a80dc38663011d40799c13f85e61dcb
UmidjonMirzaaliyev <mirzaaliyev.umidjon07@gmail.com>
2024-07-21 17:05:42 +0500

    6oy 4-dars

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import CompanyForm from "./components/CompanyForm/CompanyForm";
+function App() {
+  return <>
+    <Navbar/>
+    <CompanyForm/>
+  </>;
+}
+
+export default App;
